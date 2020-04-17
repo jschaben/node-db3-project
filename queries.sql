8de777bb9ab7eefd7cb681a41f6d92f610ed274b
Josh schaben <jschaben42@gmail.com>
2020-04-16 19:45:34 -0500

    added stretch

--- queries.sql
+++ queries.sql
@@ -2,32 +2,50 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
-select ProductName, CategoryName
-from Product
-    join Category
-    on Product.CategoryId = Category.Id
+
+SELECT ProductName, CategoryName
+FROM Product
+JOIN Category
+ON Product.CategoryId = Category.Id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
-select CompanyName, [Order].Id
-From [Order]
-    join Shipper
-    on Shipper.Id = [Order].ShipVia
-Where [Order].OrderDate < '2012-08-09'
+SELECT Id, CompanyName
+FROM [Order]
+JOIN Shipper
+ON [Order].ShipVia = Shipper.Id
+WHERE [Order].OrderDate < "2012-08-09"
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
-select ProductName, Quantity
-From Product
-    join OrderDetail
-    on Product.Id = OrderDetail.ProductId
-Where OrderDetail.OrderId = '10251'
+SELECT ProductName, Quantity 
+FROM OrderDetail
+JOIN Product
+ON Product.Id = OrderDetail.ProductId
+WHERE OrderDetail.OrderId = "10251"
+ORDER BY [Product].ProductName
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 
-select CompanyName, LastName as EmployeesLastName, [Order].Id
-From [Order]
-    join Employee
-    on Employee.Id = [Order].EmployeeId
-    join Customer
-    on Customer.Id = [Order].CustomerId
+SELECT [Order].Id as "Order Id", CompanyName as "Customer's Company Name", Employee.LastName as "Employee's Last Name"
+FROM [Order] 
+JOIN Customer
+ON [Order].CustomerId = Customer.Id
+JOIN Employee
+ON [Order].EmployeeId = Employee.Id
+
+-- STRETCH 1
+
+SELECT CategoryName, Count(*) as NumberOfProducts
+FROM [Categories]
+JOIN [Products]
+ON Categories.CategoryId = Products.CategoryId
+GROUP BY Categories.CategoryName
+
+-- STRETCH 2
+
+SELECT OrderDetails.OrderID, Count(*) as ItemCount
+FROM [OrderDetails]
+JOIN [Orders]
+ON OrderDetails.OrderId = Orders.OrderId
+GROUP BY Orders.OrderID
